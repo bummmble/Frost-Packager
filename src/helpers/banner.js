bcce75df94bffa9b3c0487f7b96a6e12428670c2
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-23 11:11:04 -0600

    Adds a helper to create a pkg banner

---
+++ src/helpers/banner.js
@@ -0,0 +1,14 @@
+export default function getBanner(pkg) {
+    let banner = `/*! ${pkg.name} v${pkg.version}`;
+
+    if (pkg.author) {
+        if (typeof pkg.author === 'object') {
+            banner += `by ${pkg.author.name} <${pkg.author.email}>`;
+        }  else if (typeof pkg.author === 'string') {
+            banner += `by ${pkg.author}`;
+        }
+    }
+
+    banner += ' */';
+    return banner;
+}
